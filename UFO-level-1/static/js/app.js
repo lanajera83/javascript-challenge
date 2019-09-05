ae0ad1c47b319f8e6bec305d49a4c88781fe29bb
Luis Najera <lanajera83@gmail.com>
2019-09-05 16:10:41 -0500

    Updated 05Sep

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,48 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  //console.log(tableData);
+
+  var filteredData = tableData.filter(file => file.datetime === inputValue);
+
+  console.log(filteredData);
+
+  // BONUS: Calculate summary statistics for the age field of the filtered data
+
+  // First, create an array with just the age values
+
+  var date = filteredData.map(i => i.datetime);
+  //var city = filteredData.map(i => i.city);
+  //var state =
+  //var country =
+  //var shape = 
+  //var duration = 
+  //var comment = 
+
+  // Then, select the unordered list element by class name
+  var list = d3.select(".summary");
+
+  // remove any children from the list to
+  //list.html("");
+
+  // append stats to the list
+  //list.append("th").text(date);
+  //list.append("th").text(city);
+  //list.append("li").text(`Mode: ${mode}`);
+  //list.append("li").text(`Variance: ${variance}`);
+  //list.append("li").text(`Standard Deviation: ${standardDeviation}`);
+  displayData(date);
+});
